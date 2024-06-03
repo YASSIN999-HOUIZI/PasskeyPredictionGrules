c581b5a41d763ed5f062469790c8a5c1de1bcc2e
HOUIZI Yassine <yassinhouizi.123@gmail.com>
2024-06-03 09:08:30 +0100

    add read from json

--- PasswordLess/passwordless.go
+++ PasswordLess/passwordless.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/csv"
+	"encoding/json"
 	"fmt"
 	"os"
 	"time"
@@ -95,80 +96,29 @@ func (df *DeviceInfo) IsCloud() bool {
 func (df *DeviceInfo) AssignOutPut(res string) string {
 	return res
 }
+func readDeviceInfoFromJSON(fileName string) ([]*DeviceInfo, error) {
+	file, err := os.Open(fileName)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
 
+	var devices []*DeviceInfo
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&devices)
+	if err != nil {
+		return nil, err
+	}
+	return devices, nil
+}
 func main() {
 
-	myFacts := [4]*DeviceInfo{
-		{
-			Label:                       "Ezzoubeir",
-			OSName:                      "Windows",
-			OSVersion:                   "10",
-			BrowserName:                 "Chrome",
-			BrowserVersion:              "90.0",
-			DeviceScreenSize_ColorDepth: [2]float64{1920, 1080},
-			IpAddress:                   "192.168.1.1",
-			DeviceFeature: DeviceFeature{
-				NavigLanguage: "English",
-				IsCMA:         true,
-				IsUVPPA:       false,
-				PasskeyType:   "local",
-			},
-			MatchOS:               true,
-			MatchDeviceProperties: false,
-		},
-		{
-			Label:                       "Anabelle",
-			OSName:                      "macOS",
-			OSVersion:                   "11.2",
-			BrowserName:                 "Safari",
-			BrowserVersion:              "14.0",
-			DeviceScreenSize_ColorDepth: [2]float64{2560, 1600},
-			IpAddress:                   "192.168.1.2",
-			DeviceFeature: DeviceFeature{
-				NavigLanguage: "French",
-				IsCMA:         false,
-				IsUVPPA:       true,
-				PasskeyType:   "device",
-			},
-			MatchOS:               true,
-			MatchDeviceProperties: true,
-		},
-		{
-			Label:                       "Jasper",
-				OSName:                      "Linux",
-			OSVersion:                   "20.04",
-			BrowserName:                 "Firefox",
-			BrowserVersion:              "89.0",
-			DeviceScreenSize_ColorDepth: [2]float64{1366, 768},
-			IpAddress:                   "192.168.1.3",
-			DeviceFeature: DeviceFeature{
-				NavigLanguage: "Spanish",
-				IsCMA:         true,
-				IsUVPPA:       true,
-				PasskeyType:   "cloud",
-			},
-			MatchOS:               false,
-			MatchDeviceProperties: false,
-		},
-		{
-			Label:                       "joe",
-			OSName:                      "Android",
-			OSVersion:                   "11",
-			BrowserName:                 "Chrome",
-			BrowserVersion:              "108",
-			DeviceScreenSize_ColorDepth: [2]float64{1080, 1920},
-			IpAddress:                   "192.168.1.4",
-			DeviceFeature: DeviceFeature{
-				NavigLanguage: "German",
-				IsCMA:         true,
-				IsUVPPA:       true,
-				PasskeyType:   "cloud",
-			},
-			MatchOS:               false,
-			MatchDeviceProperties: false,
-		},
+	// myFacts := [4]*DeviceInfo{
+		
+	devices, err := readDeviceInfoFromJSON("devices.json")
+	if err != nil {
+		panic(err)
 	}
-
 	var rules = PasskeysNonExistanceRules + DevicePropertiesMatchRules + CloudBasedPasskeysRules
 
 	drls := rules
@@ -176,7 +126,7 @@ func main() {
 	knowledgeLibrary := ast.NewKnowledgeLibrary()
 	ruleBuilder := builder.NewRuleBuilder(knowledgeLibrary)
 	bs := pkg.NewBytesResource([]byte(drls))
-	err := ruleBuilder.BuildRuleFromResource("PasswordLessRules", "0.0.2", bs)
+	err = ruleBuilder.BuildRuleFromResource("PasswordLessRules", "0.0.2", bs)
 	if err != nil {
 		panic(err)
 	}
@@ -184,7 +134,7 @@ func main() {
 	knowledgeBase, _ := knowledgeLibrary.NewKnowledgeBaseInstance("PasswordLessRules", "0.0.2")
 	engine := engine.NewGruleEngine()
 
-	for _, myFact := range myFacts {
+	for _, myFact := range devices {
 		// Initialize the intermediate predictions for each iteration
 		helper1 := &IntermediateCloudPrediction{
 			Prediction: "",
@@ -213,7 +163,7 @@ func main() {
 		}
 	}
 
-	err = writeDeviceInfoToCSV("device_info.csv", myFacts[:]) // Pass slice to avoid modifying original array
+	err = writeDeviceInfoToCSV("device_info.csv", devices[:]) // Pass slice to avoid modifying original array
 	if err != nil {
 		panic(err)
 	}
